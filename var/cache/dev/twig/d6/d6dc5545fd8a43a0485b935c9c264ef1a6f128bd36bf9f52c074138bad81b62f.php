27a8fc7a329f40e437f052fa404dca597fb7a4cc
elzo2008 <elzo2009@hotmail.fr>
2020-06-30 18:59:47 +0000

    first commit

---
+++ var/cache/dev/twig/d6/d6dc5545fd8a43a0485b935c9c264ef1a6f128bd36bf9f52c074138bad81b62f.php
@@ -0,0 +1,68 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* @Framework/Form/form_end.html.php */
+class __TwigTemplate_9027d2c79608d79b1f0a6a616310402fc5fddb95d15e37eff0057e0df1a381f1 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "@Framework/Form/form_end.html.php"));
+
+        // line 1
+        echo "<?php if (!isset(\$render_rest) || \$render_rest): ?>
+<?php echo \$view['form']->rest(\$form) ?>
+<?php endif ?>
+</form>
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "@Framework/Form/form_end.html.php";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  40 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<?php if (!isset(\$render_rest) || \$render_rest): ?>
+<?php echo \$view['form']->rest(\$form) ?>
+<?php endif ?>
+</form>
+", "@Framework/Form/form_end.html.php", "/var/www/html/sommier5/vendor/symfony/framework-bundle/Resources/views/Form/form_end.html.php");
+    }
+}
